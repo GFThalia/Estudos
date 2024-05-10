c6e7797d0875bad9c057407acb8d1aa9cb779573
GFThalia <thalia.g.f@gmail.com>
2024-05-10 16:48:30 -0300

    att desafios DIO

---
+++ DIO/BackEndComJava/Desafio Operações Bancárias Simples com Switch/SimulacaoBancaria.java
@@ -0,0 +1,48 @@
+package estudos.DIO.BackEndComJava;
+
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+
+        // Loop infinito para manter o programa em execução até que o usuário decida sair
+        while (true) {
+            // Exibir menu de opções para o usuário
+            // Ler a opção do usuário
+            int opcao = scanner.nextInt();
+
+            // Utilizar switch para executar a operação escolhida pelo usuário
+            switch (opcao) {
+                case 1:
+                    // Depositar
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:
+                    // Sacar
+                    double valorSaque = scanner.nextDouble();
+                    if (saldo >= valorSaque) {
+                        saldo -= valorSaque;
+                        System.out.println("Saldo atual: " + saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    // Consultar Saldo
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    // Encerrar
+                    System.out.println("Programa encerrado.");
+                    return; // Encerra o programa
+                default:
+                    // Opção inválida
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+}
---
+++ DIO/BackEndComJava/ProcessoSeletivo.java
@@ -0,0 +1,83 @@
+package estudos.DIO.BackEndComJava;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    
+    public static void main(String[] args){
+    
+        String[] candidatos = {"Felipe","Marcia","Julia", "Paulo", "Augusto"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("contato realizado com sucesso");
+            
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+            System.out.println("conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa.");
+        else
+            System.out.println("não conseguimos contato com "+ candidato);
+        
+    }
+
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
+    static void imprimirSelecionados(){
+        String[] candidatos = {"Felipe","Marcia","Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi "+candidato);
+        }
+    }
+
+
+
+    static void selecaoCandidatos(){
+        String[] candidatos = {"Felipe","Marcia","Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato =  candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario: " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+    
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
