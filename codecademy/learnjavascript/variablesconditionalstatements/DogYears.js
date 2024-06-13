cee3903c2353d1907e20c2cdb27b890308da0483
GFThalia <thalia.g.f@gmail.com>
2024-06-13 15:41:08 -0300

    JAVASCRIPT- INTRODUCTION: VARIABLES, CONDITIONAL STATEMENTS

---
+++ codecademy/learnjavascript/variablesconditionalstatements/DogYears.js
@@ -0,0 +1,9 @@
+const myAge = 26;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears= myAge - 2;
+laterYears *= 4;
+console.log(earlyYears, laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+myName = 'thalia'.toLowerCase();
+console.log( `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
